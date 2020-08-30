41d1d7a84fe652ceed9b9d03d1d9327a50ffb210
SemihSimsek <semih.simsek@weareyou.com>
2020-08-30 18:22:02 +0200

    changed ports

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./Utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for express config
 const publicDir = path.join(__dirname, '../public')
@@ -83,7 +84,7 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port' + port)
 })
 
